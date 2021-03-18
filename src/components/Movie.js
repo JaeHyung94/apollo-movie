e4afba31eeb9d6c000dde616a4b3160524cb1510
JaeHyung_iMac <jhpark.main@gmail.com>
2021-03-18 10:09:47 +0900

    Finishing touch

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,8 +5,8 @@ const Container = styled.div`
   height: 380px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
   border-radius: 7px;
+  overflow: hidden;
 `;
 
 const Poster = styled.div`
